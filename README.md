3032a7e289d7113326fbc0b0aa7d631cb64d6e53
Martin Mahnel <mahnel.m@gmail.com>
2019-06-20 18:57:18 +0200

    Profile adjustments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hci-demo
+## hci-demo-app
+Creates an endpoint with hci-demo-lib functionality
+
+##hci-demo-lib
+To run this from CLI, build with maven profile "cli" and spring profile "cli" (spring profile already set in application.properties).
+To run from CLI use command <code>java -jar hci-demo-lib/target/hci-demo-lib-0.1.0.jar 1</code> where you can replace <code>1</code> with userId you are looking for.
--- hci-demo-app/src/main/java/app/controller/ResultController.java
+++ hci-demo-app/src/main/java/app/controller/ResultController.java
@@ -20,39 +20,4 @@ public class ResultController {
         return typicodeService.getResult(userId);
     }
 
-    public static int rC(String s) {
-        int yearDigits = Integer.parseInt(s.substring(0, 2));
-        int year = 0;
-        int monthDigits = Integer.parseInt(s.substring(2, 4));
-        int month = 0;
-        int day = Integer.parseInt(s.substring(4, 6));
-
-        if (s.substring(6, 7).equals("/")) {
-            if (yearDigits < 54 && s.length() == 11) {
-                year = 2000 + yearDigits;
-            } else {
-                year = 1900 + yearDigits;
-            }
-        } else {
-            if (yearDigits < 54 && s.length() == 10) {
-                year = 2000 + yearDigits;
-            } else {
-                year = 1900 + yearDigits;
-            }
-        }
-
-        if (monthDigits > 0 && monthDigits <= 12) {
-            month = monthDigits;
-        } else if (monthDigits > 20 && monthDigits <= 32) {
-            month = monthDigits - 20;
-        } else if (monthDigits > 50 && monthDigits <= 62) {
-            month = monthDigits - 50;
-        } else {
-            month = monthDigits - 70;
-        }
-
-        return (day * 1000000 + month * 10000 + year);
-
-    }
-
 }
